e9a0160f66bc012a61eab4152afd2626775801f4
nehatkkhan786 <33215298+nehatkkhan786@users.noreply.github.com>
2020-01-05 16:56:24 +0530

    Update settings.py

--- gnb/settings.py
+++ gnb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's_w6)=+fmy@sg#5jofk2qzg87bb!imbkhh^m4(qfl%qq2)pv*q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
